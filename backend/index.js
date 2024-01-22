e22cc49774b31e5e67aaf7a91de916159154ff87
neoron007 <42492368+neoron007@users.noreply.github.com>
2024-01-22 21:54:03 +0530

    Update index.js
    add cors for vercel fronted app

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 // Utiles
 import connectDB from "./config/db.js";
@@ -19,6 +20,13 @@ connectDB();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: ["https://ecommerce-mern-app-eight.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
