04f17c6bca3c32810937b7a3ca4efb64e57839dd
roshan urkude <42492368+neoron007@users.noreply.github.com>
2024-01-29 13:43:09 +0530

    Update constants.js

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "ecommerce-mern-app-eu1z.vercel.app";
+export const BASE_URL = "";
 export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
 export const PRODUCT_URL = "/api/products";
