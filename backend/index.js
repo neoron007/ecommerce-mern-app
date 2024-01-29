00f9af6d405c38dc1f325378e22526ff4e63c5fc
roshan urkude <42492368+neoron007@users.noreply.github.com>
2024-01-29 11:10:46 +0530

    Update index.js
    api url added to cors policy

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: '*',
+    origin: 'ecommerce-mern-api-wine.vercel.app',
     methods: '*',
     allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version'],
     credentials: true,
