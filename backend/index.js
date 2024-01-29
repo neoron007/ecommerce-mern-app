7a6265e385500a2dcfaf6a77aea713c966a75d71
roshan urkude <42492368+neoron007@users.noreply.github.com>
2024-01-29 14:02:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'https://ecommerce-mern-app-eu1z.vercel.app',
+    origin: '*',
     methods: '*',
     allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version'],
     credentials: true,
